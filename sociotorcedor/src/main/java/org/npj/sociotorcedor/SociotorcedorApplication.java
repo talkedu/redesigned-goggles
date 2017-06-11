1df73255d82774fba75ea185a432cd01d61975d1
Eduardo Thomaz <ethomaz@id.uff.br>
2017-06-10 22:12:42 -0300

    initial commit

---
+++ sociotorcedor/src/main/java/org/npj/sociotorcedor/SociotorcedorApplication.java
@@ -0,0 +1,12 @@
+package org.npj.sociotorcedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociotorcedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociotorcedorApplication.class, args);
+	}
+}
---
+++ sociotorcedor/src/test/java/org/npj/sociotorcedor/SociotorcedorApplicationTests.java
@@ -0,0 +1,16 @@
+package org.npj.sociotorcedor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SociotorcedorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
